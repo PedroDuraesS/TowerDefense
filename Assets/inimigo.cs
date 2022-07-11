f87012319c1441da33652dc764d1664b659ab6c4
PedroDuraesS <102993693+PedroDuraesS@users.noreply.github.com>
2022-07-10 21:08:40 -0300

    ..

--- Assets/inimigo.cs
+++ Assets/inimigo.cs
@@ -7,10 +7,11 @@ public class inimigo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        UnityEngine.AI.NavMeshAgent.agente = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        UnityEngine.AI.NavMeshAgent agente = GetComponent<UnityEngine.AI.NavMeshAgent>();
         GameObject fimDoCaminho = GameObject.Find("FimDoCaminho");
         Vector3 posicaoDoFimDoCaminho = fimDoCaminho.transform.position;
         agente.SetDestination(posicaoDoFimDoCaminho);
+        
     }
 
     // Update is called once per frame
